6db0e89776816e4eb73a4ab99637db7f8cee773f
cilaky <info@cilaky.com>
2017-07-31 16:18:07 +0300

    Ajout controlleur Migration

---
+++ ImpotsTaxes/ImpotsTaxes/Controllers/MigrationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImpotsTaxes.Models;
+
+namespace ImpotsTaxes.Controllers
+{
+    public class MigrationController : Controller
+    {
+        //
+        // GET: /Migration/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Assujetti()
+        {
+            //List<Person> lst = new List<Person>();
+            Liste lst = new Liste();
+            ViewBag.Assujetti = lst.AssujettiNoteMig();
+            return View();
+        }
+
+        public ActionResult SelectionAssujetti()
+        {
+            return View();
+        }
+
+        public ActionResult NotePerception()
+        {
+            return View();
+        }
+    }
+}
